d4407e280df7f14630fa3021a3914454e7605516
olive <blueblueww@yahoo.co.jp>
2024-11-01 17:03:17 +0900

    241101 sitemap修正

--- WordPressTheme/page-sitemap.php
+++ WordPressTheme/page-sitemap.php
@@ -35,91 +35,91 @@ Template Name: sitemap
 	</div>
 
 	<!-- sitemap -->
-	<section class="sitemap-body layout-body fish-icon">
+	<section class="sitemap-body layout-body layout-body--sitemap">
 		<div class="sitemap-body__inner inner">
-			<nav class="sitemap-body__nav footer-nav footer-nav--sitemap">
-				<div class="footer-nav__left">
-					<ul class="footer-nav__left-items">
-						<li class="footer-nav__left-item">
+			<nav class="sitemap-body__nav sitemap-body-nav fish-icon">
+				<div class="sitemap-body-nav__left">
+					<ul class="sitemap-body-nav__left-items">
+						<li class="sitemap-body-nav__left-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>campaign">キャンペーン</a>
 						</li>
-						<li class="footer-nav__left-item">
+						<li class="sitemap-body-nav__left-item">
 							<a href="">ライセンス取得</a>
 						</li>
-						<li class="footer-nav__left-item">
+						<li class="sitemap-body-nav__left-item">
 							<a href="">貸切体験ダイビング</a>
 						</li>
-						<li class="footer-nav__left-item">
+						<li class="sitemap-body-nav__left-item">
 							<a href="">ナイトダイビング</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__left-items">
-						<li class="footer-nav__left-item">
+					<ul class="sitemap-body-nav__left-items">
+						<li class="sitemap-body-nav__left-item">
 							<a href="<?php echo esc_url( home_url('/')); ?>about">私たちについて</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__left-items">
-						<li class="footer-nav__left-item">
+					<ul class="sitemap-body-nav__left-items">
+						<li class="sitemap-body-nav__left-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>information">ダイビング情報</a>
 						</li>
-						<li class="footer-nav__left-item">
+						<li class="sitemap-body-nav__left-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>information?no=license">ライセンス講習</a>
 						</li>
-						<li class="footer-nav__left-item">
+						<li class="sitemap-body-nav__left-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>information?no=experience">体験ダイビング</a>
 						</li>
-						<li class="footer-nav__left-item">
+						<li class="sitemap-body-nav__left-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>information?no=fan">ファンダイビング</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__left-items">
-						<li class="footer-nav__left-item">
+					<ul class="sitemap-body-nav__left-items">
+						<li class="sitemap-body-nav__left-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>blog">ブログ</a>
 						</li>
 					</ul>
 				</div>
-				<div class="footer-nav__right">
-					<ul class="footer-nav__right-items">
-						<li class="footer-nav__right-item">
+				<div class="sitemap-body-nav__right">
+					<ul class="sitemap-body-nav__right-items">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>voice">お客様の声</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__right-items">
-						<li class="footer-nav__right-item">
+					<ul class="sitemap-body-nav__right-items">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>price">料金一覧</a>
 						</li>
-						<li class="footer-nav__right-item">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>price#license">ライセンス講習</a>
 						</li>
-						<li class="footer-nav__right-item">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>price#experience">体験ダイビング</a>
 						</li>
-						<li class="footer-nav__right-item">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>price#fan">ファンダイビング</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__right-items">
-						<li class="footer-nav__right-item">
+					<ul class="sitemap-body-nav__right-items">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>faq">よくある質問</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__right-items">
-						<li class="footer-nav__right-item">
+					<ul class="sitemap-body-nav__right-items">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>privacy-policy">プライバシー<br class="u-mobile">ポリシー</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__right-items">
-						<li class="footer-nav__right-item">
+					<ul class="sitemap-body-nav__right-items">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>terms-of-service">利用規約</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__right-items">
-						<li class="footer-nav__right-item">
+					<ul class="sitemap-body-nav__right-items">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">お問い合わせ</a>
 						</li>
 					</ul>
-					<ul class="footer-nav__right-items">
-						<li class="footer-nav__right-item">
+					<ul class="sitemap-body-nav__right-items">
+						<li class="sitemap-body-nav__right-item">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>sitemap">サイトマップ</a>
 						</li>
 					</ul>
